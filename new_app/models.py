30ecd96048e52db38fff0d7b418b3c78ac357f0c
akshaycs435 <anaghasasidharan38@gmail.com>
2024-01-14 00:33:44 +0530

    include templates

--- new_app/models.py
+++ new_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+# from django.auth import User
 # Create your models here.
+# class Users(models.Model):
+#     first_name=models.CharField(max_length=20)
+#     last_name=models.CharField(max_length=20)
+#     email_address=models.EmailField(max_length=50)
+#     password=models.CharField(max_length=20)
+#     confirm_password=models.CharField(max_length=20)
+#     status=models.BooleanField(default=False)
--- new_app/views.py
+++ new_app/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 
 def index(request):
+    return render(request,'base.html')
+
+def signup(request):
+
+    if request.method=="POST":
+        username=request.POST['username']
+        first_name=request.POST['first_name']
+        last_name=request.POST['last_name']
+        email_id=request.POST['email']
+        password=request.POST['password1']
+        confirm_password=request.POST['password2']
+
+        my_user=User.objects.create_user(username,email_id,password)
+        my_user.first_name=first_name
+        my_user.last_name=last_name
+
+        my_user.save()
+
+        messages.success(request,"Your Account has been successfully created")
+
+        status=['status']
+
+        return redirect('signin')
+
+    return render(request,'signup.html')
+
+def signin(request):
+    return render(request,'signin.html')
+
+def login(request):
     return render(request,'login.html')
+
+def signout(request):
+    pass
+    # return render(request,'signo.html')
