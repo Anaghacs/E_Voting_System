31dec7fea2b3790fad5e1ece2a4bd215d04b648f
akshaycs435 <anaghasasidharan38@gmail.com>
2024-01-15 00:07:36 +0530

    corresponding web pages arrangement and authentication user and admin

--- new_app/urls.py
+++ new_app/urls.py
@@ -6,7 +6,9 @@ urlpatterns=[
     path('signin',views.signin,name="signin"),
     path('signout',views.signout,name="signout"),
     path('login',views.login,name="login"),
+    path('user_login',views.user_login,name="user_login"),
     path('admin_home',views.admin_home,name="admin_home"),
+    path('user_home',views.user_home,name="user_home"),
     path('view_users',views.view_users,name="views_users"),
     path('delete/<int:id>/',views.delete,name="delete")
 
--- new_app/views.py
+++ new_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
-from django.contrib import messages
+from django.contrib import messages,auth
 from django.contrib.auth import authenticate,login
 from django.contrib.auth import logout
 
@@ -20,6 +20,13 @@ def signup(request):
         password=request.POST['password1']
         confirm_password=request.POST['password2']
 
+        if User.objects.filter(username=username):
+            messages.error(request,"Username already exist! Please try some other username")
+            return redirect('base')
+        if password !=confirm_password:
+            messages.error(request,"Password didn't match !")
+            return redirect('base')
+
         my_user=User.objects.create_user(username,email_id,password)
         my_user.first_name=first_name
         my_user.last_name=last_name
@@ -37,30 +44,45 @@ def signin(request):
     render(request,'base.html')
 
 def login(request):
-    
+    # print("hello")
     if request.method=='POST':
         username=request.POST['username']
         pass1=request.POST['password1']
+        # print("username--------------------------------------------------------",username)
+        # print(pass1)
 
-        user=authenticate(username=username,pasword=pass1)
+        user=auth.authenticate(request,username=username,password=pass1)
 
         if user is not None:
-            login(request,user)
-
+            # print("user**************************")
+            auth.login(request,user)
+            if user.is_superuser:
+                # print("admin")
+                return render(request,'admin_home.html')
+            else:
+                # print("user")
+                return render(request,'user_home.html')
         else:
-            messages.error(request,"Bad Credential")
-            fname=user.first_name
-            return redirect(request,'admin_home.html',{'fname':fname})
-    
+
+            # print("no user111111111111111111")
+            messages.error(request,"Invalid login credentials")
+            return redirect('login')
+        
     return render(request,'login.html')
 
 def signout(request):
     logout(request)
     messages.success(request,"Logged Out Successfully!")
-    return redirect("base")
+    return redirect("index")
 
 def admin_home(request):
     return render(request,'admin_home.html')
+
+def user_login(request):
+    return render(request,"user_home.html")
+
+def user_home(request):
+    return render(request,'user_home.html')
 
 def view_users(request):
     user=User.objects.filter(is_staff=False)
@@ -70,3 +92,7 @@ def delete(request,id):
     user=User.objects.get(id=id)
     user.delete()
     return redirect("view_users")
+
+def update(request,id):
+    user=User.objects.get(id=id)
+    return render(request,'update.html',{'user':user})
